96a1521bcb9eb2a6fb8576154ca3cb480b233e39
Rakotoarilala51 <maitreaina51@gmail.com>
2025-07-28 09:46:17 +0300

    api singleton

--- api.go
+++ api.go
@@ -1,1 +1,18 @@
 package forkyou
+
+import (
+	"github.com/Rakotoarilala51/rin"
+	"github.com/spf13/viper"
+)
+
+var api *rin.API
+
+func GithubAPI() *rin.API {
+	if api ==nil{
+		api = rin.NewApi("https://api.github.com")
+		token := viper.GetString("token")
+		api.SetAuth(rin.NewAuthToken(token))
+		//api.AddRessource()
+	}
+	return api
+}
