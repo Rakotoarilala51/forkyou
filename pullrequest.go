c10b2a37b13e0387faec3d65e9eee4dc2ae4dd2a
Rakotoarilala51 <maitreaina51@gmail.com>
2025-08-01 16:48:07 +0300

    aiza izy zany

--- pullrequest.go
+++ pullrequest.go
@@ -1,7 +1,22 @@
 package forkyou
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"log"
+	"strings"
 
+	"github.com/spf13/cobra"
+)
+
+var (
+	destRepo string
+	sourceRepo string
+	pullRequestTitle string
+	pullRequestMessage string
+	SourceBranch string
+	destBranch string
+
+)
 type PullRequestPayload struct{
 	Title string `json:"title"`
 	Message string `json:"message"`
@@ -16,6 +31,31 @@ var PullRequestCmd =&cobra.Command{
 	Use: "pullrequest",
 	Short: "Create a Pull Request",
 	Run: func(cmd *cobra.Command, args []string) {
-
+		if err := CreatePullRequest(); err != nil{
+			log.Fatalln("Failed to create pull request: ", err)
+		}
 	},
+}
+
+func CreatePullRequest() error{
+	sourcesValues := strings.Split(sourceRepo, ":")
+	if !(len(sourcesValues)==1 || len(sourcesValues) ==2){
+		return fmt.Errorf("source repository must in the format [owner:]branch got  %v", sourceRepo)
+	}
+	destBranchValues := strings.Split(destRepo,":")
+	if len(destBranchValues) != 2{
+		return fmt.Errorf("destination repository must be in format in the format owner/project:branch got %v", destRepo)
+	}
+	destValues := strings.Split(destBranchValues[0], "/")
+	if len(destValues)!=2 {
+		return fmt.Errorf("destination repository must be in the format owner/project:branch got %v", destRepo)
+	}
+	return nil
+}
+
+func init(){
+	PullRequestCmd.Flags().StringVarP(&sourceRepo, "source", "s", "", "source repository")
+	PullRequestCmd.Flags().StringVarP(&destRepo,  "destination", "d", "", "destination repository")
+	PullRequestCmd.Flags().StringVarP(&pullRequestTitle, "title", "t", "Basic pull request", "pull request title")
+	PullRequestCmd.Flags().StringVarP(&pullRequestMessage, "message", "m", "", "pull request message")
 }
