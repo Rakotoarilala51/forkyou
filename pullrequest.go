ed1f1b834929f97592a9d0bca38b4ca1686fa615
Rakotoarilala51 <maitreaina51@gmail.com>
2025-08-01 16:22:47 +0300

    pull request

--- pullrequest.go
+++ pullrequest.go
@@ -1,1 +1,21 @@
 package forkyou
+
+import "github.com/spf13/cobra"
+
+type PullRequestPayload struct{
+	Title string `json:"title"`
+	Message string `json:"message"`
+	SourceBranch string `json:"head"`
+	DestBranch string `json:"base"`
+	Modify string `json:"maintener_can_modify"`
+}
+type PullRequestResponse struct{
+	URL string `json:"html_url"`
+}
+var PullRequestCmd =&cobra.Command{
+	Use: "pullrequest",
+	Short: "Create a Pull Request",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
