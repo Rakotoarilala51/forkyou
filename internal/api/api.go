28cf6f352dc789a13bd4da81b9affb02106a65f4
Aina-Akata <aina-akata@goavna.com>
2026-01-31 17:15:50 +0300

    refactor(api): refactor api and resources

---
+++ internal/api/api.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"github.com/Rakotoarilala51/rin"
+	"github.com/spf13/viper"
+)
+
+var api *rin.API
+
+func GithubAPI() *rin.API {
+	if api == nil {
+		api = rin.NewApi("https://api.github.com")
+		token := viper.GetString("token")
+		api.SetAuth(rin.NewAuthToken(token))
+		api.AddRessource("fork", GetForkRessource())
+		api.AddRessource("search", GetSearchRessource())
+		api.AddRessource("docs", GetReadmeRessource())
+	}
+	return api
+}
