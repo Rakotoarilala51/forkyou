60beec05717d7691ee6602bbc2fad8a583ca260f
Aina-Akata <aina-akata@goavna.com>
2026-02-01 15:32:18 +0300

    feat(config): getting token from config package

--- internal/cli/config/get_token.go
+++ internal/cli/config/get_token.go
@@ -1,9 +1,16 @@
 package config
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/Rakotoarilala51/forkyou/internal/config"
+	"github.com/spf13/cobra"
+)
 
 var getTokenCmd = &cobra.Command{
 	Use:   "get-token",
 	Short: "print existing token",
-	Run:   func(cmd *cobra.Command, args []string) {},
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(config.GetToken())
+	},
 }
